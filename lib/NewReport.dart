2ba3c99365b318189d2e55ae872fced0b920bf2e
maansi-1608 <maansisinha92@gmail.com>
2021-08-11 18:58:01 +0530

    added comments

--- lib/NewReport.dart
+++ lib/NewReport.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
-
+//this file is for adding a patients report
 class NewReport extends StatefulWidget {
 @override
 _NewReportState createState() => _NewReportState();
